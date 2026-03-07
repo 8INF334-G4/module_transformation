96e7074307bec527d07c3328cf5ee3a4987d4473
Mathieu <mathieu.borel@epitech.eu>
2026-03-07 22:21:35 +0000

    Mbo/#3/reading subsystem (#14)
    * feat(Product): add product class
    
    Signed-off-by: m-brl <mathieu.borel@epitech.eu>
    
    * feat(mock): add ABC mock
    
    Signed-off-by: m-brl <mathieu.borel@epitech.eu>
    
    * feat(interface): add reader interface
    feat(ABC Reader): add ABC reader
    
    Signed-off-by: m-brl <mathieu.borel@epitech.eu>
    
    ---------
    
    Signed-off-by: m-brl <mathieu.borel@epitech.eu>

---
+++ src/main/java/org/groupe4/ABCReader.java
@@ -0,0 +1,43 @@
+package org.groupe4;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.groupe4.mock.MockABC;
+
+import java.util.Map;
+
+public class ABCReader {
+    private MockABC _mockABC;
+    private int _currentId = 0;
+
+    public ABCReader(MockABC mockABC) {
+        _mockABC = mockABC;
+        _currentId = 0;
+    }
+
+    public ABCReader(MockABC mockabc, int startId) {
+        _mockABC = mockabc;
+        _currentId = startId;
+    }
+
+    public Product read(int id) {
+        Product product = new Product();
+        String report = _mockABC.GenerateReport(id);
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode rootNode = mapper.readTree(report);
+            rootNode.fields().forEachRemaining(elt -> {
+                product.setAttribute(elt.getKey(), elt.getValue().asText());
+            });
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to read product " + id);
+        }
+        return product;
+    }
+
+    public Product readNext() {
+        return read(_currentId++);
+    }
+
+}
---
+++ src/main/java/org/groupe4/InputReader.java
@@ -0,0 +1,7 @@
+package org.groupe4;
+
+import org.groupe4.Product;
+
+public interface InputReader {
+    Product readNext();
+}
