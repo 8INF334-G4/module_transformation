7840b54dbb8ad9657a729fc3c48bc35a1cdd932e
Mathieu <mathieu.borel@epitech.eu>
2026-03-10 16:29:17 +0000

    feat(logger): add logger (#13)
    Co-authored-by: Dylane <kenmatiotenko@gmail.com>

--- src/main/java/org/groupe4/Main.java
+++ src/main/java/org/groupe4/Main.java
@@ -1,7 +1,6 @@
 package org.groupe4;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello world!");
+    public static void main(String[] args) {      
     }
 }
